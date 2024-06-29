e659668448d3075c75ec3472034d5553d0127ba9
ssher <ssher250110@yandex.ru>
2024-06-29 20:39:57 +0300

    added model habit

--- habits/models.py
+++ habits/models.py
@@ -1,3 +1,40 @@
+from datetime import timedelta
+
+from django.conf import settings
+from django.core.validators import MaxValueValidator
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Habit(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Создатель привычки',
+                             help_text='Укажите создателя привычки')
+    location = models.CharField(max_length=100, **NULLABLE, verbose_name='Место выполнения привычки',
+                                help_text='Укажите место выполнения привычки')
+    time = models.TimeField(verbose_name='Время выполнения привычки', help_text='Укажите время выполнения привычки')
+    action = models.CharField(max_length=150, verbose_name='Действие привычки', help_text='Укажите действие привычки')
+    is_pleasant_habit = models.BooleanField(default=False, verbose_name='Приятная привычка',
+                                            help_text='Укажите, приятная ли привычка')
+    related_habit = models.ForeignKey('self', on_delete=models.SET_NULL, **NULLABLE,
+                                      verbose_name='Связанная полезная привычка',
+                                      help_text='Укажите связанную полезную привычку')
+    periodicity = models.PositiveIntegerField(default=1, verbose_name='Периодичность выполнения привычки',
+                                              help_text='Укажите периодичность выполнения привычки')
+    award = models.CharField(max_length=150, **NULLABLE, verbose_name='Вознаграждение',
+                             help_text='Укажите вознаграждение')
+    duration = models.DurationField(validators=[MaxValueValidator(timedelta(seconds=120))],
+                                    verbose_name='Время на выполнение привычки',
+                                    help_text='Укажите время на выполнение привычки')
+    is_publication = models.BooleanField(default=False, verbose_name='Признак публикации',
+                                         help_text='Укажите признак публикации')
+
+    def __str__(self):
+        return f'Привычка: {self.action}, в {self.time}, место: {self.location}'
+
+    class Meta:
+        verbose_name = 'Привычка'
+        verbose_name_plural = 'Привычки'
+        ordering = ['location', ]
