eda6e55992f68a770d8995c0b32f765b6ea75bfe
ssher <ssher250110@yandex.ru>
2024-06-29 19:31:23 +0300

    editing namespace users

--- config/urls.py
+++ config/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('habits.urls', namespace='habits')),
-    path('users/', include('users.urls', namespace='habits')),
+    path('users/', include('users.urls', namespace='users')),
 ]
