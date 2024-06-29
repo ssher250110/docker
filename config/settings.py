d27df0771682201e92818f1a5ebd94598e4ea5ca
ssher <ssher250110@yandex.ru>
2024-06-29 19:18:28 +0300

    added path static and media

--- config/settings.py
+++ config/settings.py
@@ -90,7 +90,11 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (BASE_DIR / 'static',)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
