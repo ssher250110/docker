a3fcd3fed982c5eaf0812728edf2a1ab351f98f7
ssher <ssher250110@yandex.ru>
2024-06-30 21:44:46 +0300

    added def perform_create

--- users/views.py
+++ users/views.py
@@ -8,3 +8,7 @@ class UserCreateAPIView(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
