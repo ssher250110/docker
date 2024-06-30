ae3673890deca6f2760a8dd66bcf60c11990303b
ssher <ssher250110@yandex.ru>
2024-06-30 15:12:59 +0300

    added path view genetics

--- habits/urls.py
+++ habits/urls.py
@@ -1,7 +1,15 @@
+from django.urls import path
+
 from habits.apps import HabitsConfig
+from habits.views import HabitCreateAPIView, HabitListAPIView, HabitRetrieveAPIView, HabitUpdateAPIView, \
+    HabitDestroyAPIView
 
 app_name = HabitsConfig.name
 
 urlpatterns = [
-
+    path('habit/create/', HabitCreateAPIView.as_view(), name='habit-create'),
+    path('habit/', HabitListAPIView.as_view(), name='habit-list'),
+    path('habit/<int:pk>/', HabitRetrieveAPIView.as_view(), name='habit-retrieve'),
+    path('habit/<int:pk>/update/', HabitUpdateAPIView.as_view(), name='habit-update'),
+    path('habit/<int:pk>/destroy/', HabitDestroyAPIView.as_view(), name='habit-destroy'),
 ]
