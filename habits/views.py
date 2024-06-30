b9eeb38bfe2502253678d6330772c812e73c18b1
ssher <ssher250110@yandex.ru>
2024-06-30 22:36:59 +0300

    added permission_classes ApiView

--- habits/views.py
+++ habits/views.py
@@ -2,6 +2,7 @@ from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView,
 from rest_framework.permissions import AllowAny
 
 from habits.models import Habit
+from habits.permissions import IsOwner
 from habits.serializers import HabitSerializer
 
 
@@ -13,21 +14,25 @@ class HabitCreateAPIView(CreateAPIView):
 class HabitListAPIView(ListAPIView):
     queryset = Habit.objects.all()
     serializer_class = HabitSerializer
+    permission_classes = [IsOwner]
 
 
 class HabitRetrieveAPIView(RetrieveAPIView):
     queryset = Habit.objects.all()
     serializer_class = HabitSerializer
+    permission_classes = [IsOwner]
 
 
 class HabitUpdateAPIView(UpdateAPIView):
     queryset = Habit.objects.all()
     serializer_class = HabitSerializer
+    permission_classes = [IsOwner]
 
 
 class HabitDestroyAPIView(DestroyAPIView):
     queryset = Habit.objects.all()
     serializer_class = HabitSerializer
+    permission_classes = [IsOwner]
 
 
 class PublicHabitListAPIView(ListAPIView):
