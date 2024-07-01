73ff1342df52e8f48d23721148c1c9e68ff9dfe3
ssher <ssher250110@yandex.ru>
2024-07-01 03:05:09 +0300

    added validators

--- habits/validators.py
+++ habits/validators.py
@@ -1,6 +1,34 @@
 from rest_framework.serializers import ValidationError
 
 
+class RelatedHabitValidator:
+    def __init__(self, field):
+        self.field = field
+
+    def __call__(self, value):
+        related_habit = value.get(self.field)
+        is_pleasant_habit = value.get('is_pleasant_habit')
+        if related_habit and is_pleasant_habit:
+            raise ValidationError('У приятной привычки не может быть связанной привычки.')
+        elif related_habit and not related_habit.is_pleasant_habit:
+            raise ValidationError('В связанные привычки могут попадать только привычки с признаком приятной привычки.')
+
+
+class AwardHabitValidator:
+
+    def __init__(self, field):
+        self.field = field
+
+    def __cal__(self, value):
+        award = value.get(self.field)
+        is_pleasant_habit = value.get('is_pleasant_habit')
+        related_habit = value.get('related_habit')
+        if award and is_pleasant_habit:
+            raise ValidationError('У приятной привычки не может быть вознаграждения')
+        elif award and related_habit:
+            raise ValidationError('Нельзя одновременно выбрать связанную привычку и вознаграждение.')
+
+
 class TimeCompleteHabitValidator:
 
     def __init__(self, field):
@@ -21,5 +49,3 @@ class PeriodicityHabitValidator:
         periodicity = value.get(self.field)
         if periodicity < 7:
             raise ValidationError('Нельзя выполнять привычку реже, чем 1 раз в 7 дней.')
-
-
