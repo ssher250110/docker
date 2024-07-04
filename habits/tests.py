0e211bec509e2329b1d47a8bbe3bf65c219a0aa9
ssher <ssher250110@yandex.ru>
2024-07-05 02:12:12 +0300

    editing double quotes for single quotes

--- habits/tests.py
+++ habits/tests.py
@@ -11,51 +11,51 @@ class HabitTestCase(APITestCase):
 
     def setUp(self):
         """Тестовые данные для тестов"""
-        self.user = User.objects.create(email="admin@admin.com")
+        self.user = User.objects.create(email='admin@admin.com')
         self.client.force_authenticate(user=self.user)
         self.habit = Habit.objects.create(
             owner=self.user,
-            time="22:00",
-            action="run",
+            time='22:00',
+            action='run',
         )
 
     def test_habit_retrieve(self):
         """Тестирование просмотра одной привычки"""
-        url = reverse("habits:habit-retrieve", args=(self.habit.pk,))
+        url = reverse('habits:habit-retrieve', args=(self.habit.pk,))
         response = self.client.get(url)
         data = response.json()
         self.assertEqual(
             response.status_code, status.HTTP_200_OK
         )
         self.assertEqual(
-            data.get("action"), self.habit.action
+            data.get('action'), self.habit.action
         )
 
     def test_habit_list(self):
         """Тестирование просмотра списка привычек"""
-        url = reverse("habits:habit-list")
+        url = reverse('habits:habit-list')
         response = self.client.get(url)
         data = response.json()
         self.assertEqual(
             Habit.objects.all().count(), 1
         )
         result = {
-            "count": 1,
-            "next": None,
-            "previous": None,
-            "results": [
+            'count': 1,
+            'next': None,
+            'previous': None,
+            'results': [
                 {
-                    "id": self.habit.pk,
-                    "owner": self.user.pk,
-                    "location": None,
-                    "time": self.habit.time + ":00",
-                    "action": self.habit.action,
-                    "is_pleasant_habit": False,
-                    "related_habit": None,
-                    "periodicity": 1,
-                    "award": None,
-                    "time_complete": 1,
-                    "is_publication": False,
+                    'id': self.habit.pk,
+                    'owner': self.user.pk,
+                    'location': None,
+                    'time': self.habit.time + ':00',
+                    'action': self.habit.action,
+                    'is_pleasant_habit': False,
+                    'related_habit': None,
+                    'periodicity': 1,
+                    'award': None,
+                    'time_complete': 1,
+                    'is_publication': False,
                 }
             ]
         }
@@ -68,11 +68,11 @@ class HabitTestCase(APITestCase):
 
     def test_habit_create(self):
         """Тестирование создания привычки"""
-        url = reverse("habits:habit-create")
+        url = reverse('habits:habit-create')
         data = {
-            "owner": self.user.pk,
-            "time": "23:00",
-            "action": "run 2"
+            'owner': self.user.pk,
+            'time': '23:00',
+            'action': 'run 2'''
         }
         response = self.client.post(url, data)
         self.assertEqual(
@@ -84,15 +84,15 @@ class HabitTestCase(APITestCase):
 
     def test_habit_update(self):
         """Тестирование обновления привычки"""
-        url = reverse("habits:habit-update", args=(self.habit.pk,))
+        url = reverse('habits:habit-update', args=(self.habit.pk,))
         data = {
-            "time": "21:00",
-            "action": "sleep"
+            'time': '21:00',
+            'action': 'sleep'
         }
         response = self.client.patch(url, data)
         data = response.json()
         self.assertEqual(
-            data.get("action"), "sleep"
+            data.get('action'), 'sleep'
         )
         self.assertEqual(
             response.status_code, status.HTTP_200_OK
@@ -100,7 +100,7 @@ class HabitTestCase(APITestCase):
 
     def test_habit_delete(self):
         """Тестирование удаления привычки"""
-        url = reverse("habits:habit-destroy", args=(self.habit.pk,))
+        url = reverse('habits:habit-destroy', args=(self.habit.pk,))
         response = self.client.delete(url)
         self.assertEqual(
             Habit.objects.all().count(), 0
