7b3f6e03b21faeeb6245027b3bdbbee08dad2ba2
ssher <ssher250110@yandex.ru>
2024-07-04 23:30:29 +0300

    added test_habit_retrieve

--- habits/tests.py
+++ habits/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from habits.models import Habit
+from users.models import User
+
+
+class HabitTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(email="admin@admin.com")
+        self.client.force_authenticate(user=self.user)
+        self.habit = Habit.objects.create(
+            owner=self.user,
+            time="22:00",
+            action="run",
+        )
+
+    def test_habit_retrieve(self):
+        url = reverse("habits:habit-retrieve", args=(self.habit.pk,))
+        response = self.client.get(url)
+        data = response.json()
+        self.assertEqual(
+            response.status_code, status.HTTP_200_OK
+        )
+        self.assertEqual(
+            data.get("action"), self.habit.action
+        )
